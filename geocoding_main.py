ab41c794ac3b4952a4ae6fc3f13cb95807aeb271
Syed Sadiqu Hussain <syedsadiquh@gmail.com>
2024-02-28 00:48:25 +0530

    Update: Added Code Documentaion

---
+++ geocoding_main.py
@@ -0,0 +1,12 @@
+''' 
+This python scrpit is for geocoding and reverse geocoding using Map Maker (My Maps Inc) Geocoding API.
+
+This script allows for address to be coded to geometric lattitude and longitude i.e. geocoding 
+and also geometric lattitude and longitude to address i.e. reverse geocoding.
+
+The address is inputed into the program and the result is shown by the program.
+The free tier of Map Maker Geocoding API is set to 1 api-call per second or 1,000,000/month
+to get more upgrade to Paid plans as per your requirements.
+
+NOTE: - Make sure to put your api key before running the code. 
+'''
