88655ac9772d5ab990f5caf4082eec7a4bb51fd7
Syed Sadiqu Hussain <syedsadiquh@gmail.com>
2024-03-18 20:46:06 +0530

    Added: TreeView for data and No data Label when no data is Available.

--- History_Screen.py
+++ History_Screen.py
@@ -1,9 +1,39 @@
 import tkinter as tk
+from tkinter import ttk
+
+from dbConnector import DBConnect
 
 
 # History Window for the Queries along with the responses.
 class HistoryWindow:
     def __init__(self, master):
         self.master = master
-        self.label = tk.Label(master=self.master, text="HELLO WORLD", font=('Helvetica', 21))
+        self.label = tk.Label(
+            master=self.master,
+            text="History",
+            font=('Helvetica', 21),
+            pady=15
+        )
         self.label.pack()
+
+        # Database connection
+        db = DBConnect()
+        db.create_table()
+        self.data = db.read_all()
+        if len(self.data) != 0:
+            self.tree = ttk.Treeview(master=self.master, columns=("c1", "c2", "c3"), show='headings')
+            self.tree.column("#1", anchor='w', minwidth=40, width=50)
+            self.tree.heading("#1", text="Sl. No.", anchor=tk.CENTER)
+            self.tree.column("#2", anchor=tk.CENTER)
+            self.tree.heading("#2", text="Query")
+            self.tree.column("#3", anchor=tk.CENTER)
+            self.tree.heading("#3", text="Response")
+            self.tree.pack()
+            for row in self.data:
+                print(row)
+                self.tree.insert("", tk.END, values=row)
+        else:
+            self.empty_label = tk.Label(self.master, text="No Data Found", font=('Helvetica bold', 14))
+            self.empty_label.pack()
+
+        db.close_connection()
